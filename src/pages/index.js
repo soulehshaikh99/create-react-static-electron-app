f048686b81d489faeccbb21168594d78cffa83fa
soulehshaikh99 <soulehshaikh99@gmail.com>
2020-08-12 20:06:43 +0530

    :file_cabinet: Source Code Directory

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>Welcome to React-Static</h1>
+    <img src="/logo.svg" alt="logo" style={{height: '40vmin'}} />
+  </div>
+)
